07f91b84f513112de791b8b0133ffeca10e721d5
2130845h <114968658+2130845h@users.noreply.github.com>
2023-02-19 17:37:30 +0900

    12-13 change

--- ad_delete.js
+++
@@ -1,5 +0,0 @@
-let ads = document.querySelectorAll(".chiePcMastheadtarget_modules", "yjAdImage");
-for(let i = 0; i < ads.length; i++){
-  let e = ads[i];
-  e.style.display = 'none';
-}
---
+++ simple.js
@@ -0,0 +1,6 @@
+let waste = document.querySelectorAll("SearchTop_logo");
+for(let i = 0; i < waste.length; i++){
+  let e = waste[i];
+  e.style.display = 'none';
+}
+// ".SearchTop_header", "SearchTop_logo", "sw-SearchTopWord target_modules", "target_modules", "Footer_target_modules"
